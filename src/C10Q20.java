d8acd472b2ebc6c04a29519a06dd825f15161b84
RyanP <rgp2509@email.vccs.edu>
2017-10-17 17:54:36 -0400

    1

--- src/C10Q20.java
+++ src/C10Q20.java
@@ -1,3 +1,11 @@
+//
+//(Approximate e) Programming Exercise 5.26 approximates e using the following series
+// : e = 1 + 1/1! + 1/2! + 1/3! + 1/4! + c + 1/i!
+//
+//In order to get better precision, use BigDecimal with 25 digits of precision in the computation.
+// Write a program that displays the e value for i = 100, 200, . . ., and 1000.
+
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
